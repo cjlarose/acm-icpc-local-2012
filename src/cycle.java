2f995c5bd0d3481c45021aa9a99b23f19abf7177
Chris LaRose <cjlarose@gmail.com>
2012-09-22 21:46:22 -0700

    cycle::get_permutations now returns List<int[]> instead of List<List<Integer>> to save memory

--- src/cycle.java
+++ src/cycle.java
@@ -3,59 +3,60 @@ import java.util.List;
 import java.util.Scanner;
 
 public class cycle {
-	
+
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
-		
+
 		int problems = s.nextInt();
 		int results[] = new int[problems];
 		int num, size, max, temp, tempMax;
-		
-		for(int i = 0; i < problems; i++) {
+
+		for (int i = 0; i < problems; i++) {
 			num = s.nextInt();
 			size = s.nextInt();
 			max = s.nextInt();
-			
-			List<List<Integer>> perms = get_permutations(size);
+
+			List<int[]> perms = get_permutations(size);
 			for (int k = 0; k < perms.size(); k++) {
-				List<Integer> permutation = perms.get(k);
-				temp = permutation.get(0);
-				for(int j = 0; j <= permutation.size(); j++) {
-					if(temp == max) {
+				int[] permutation = perms.get(k);
+				temp = permutation[0];
+				for (int j = 0; j <= permutation.length; j++) {
+					if (temp == max) {
 						results[i]++;
 						break;
 					}
-					temp = permutation.get(temp-1);
+					temp = permutation[temp - 1];
 				}
 			}
 		}
-		
-		for(int i = 0; i < problems; i++) {
-			System.out.println((i+1) + " " + results[i]);
+
+		for (int i = 0; i < problems; i++) {
+			System.out.println((i + 1) + " " + results[i]);
 		}
 	}
-	
-	private static List<List<Integer>> get_permutations(int n) {
-		List<List<Integer>> results = new ArrayList<List<Integer>>();
+
+	private static List<int[]> get_permutations(int n) {
+		List<int[]> results = new ArrayList<int[]>();
 		if (n == 1) {
-			List<Integer> result = new ArrayList<Integer>();
-			result.add(1);
-			results.add(result);
+			results.add(new int[] {1});
 		} else {
-			List<List<Integer>> sub_r = get_permutations(n-1); // [1,2],[2,1]
+			List<int[]> sub_r = get_permutations(n - 1); // [1,2],[2,1]
 			for (int i = 0; i < sub_r.size(); i++) {
-				List<Integer> sub_list = sub_r.get(i); // [2,1]
+				int[] sub_list = sub_r.get(i); // [2,1]
 				for (int j = 0; j < n; j++) {
-					List<Integer> result = new ArrayList<Integer>(); // [3,2,1]
-					result.addAll(sub_list);
-					result.add(j, n);
+					int[] result = new int[n]; // [3,2,1]
+					for (int k = 0; k < j; k++) {
+						result[k] = sub_list[k];
+					}
+					result[j] = n;
+					for (int k = j+1; k < n; k++) {
+						result[k] = sub_list[k-1];
+					}
 					results.add(result);
 				}
 			}
 		}
 		return results;
 	}
-	
-	
 
 }
